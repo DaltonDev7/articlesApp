5a41be83571c1b0e3cec5e4f5d64bdbe794779e2
DaltonDev7 <daltontejada28@gmail.com>
2024-07-14 11:41:33 -0400

    refactor: metodo getarticles

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -8,29 +8,51 @@ const prisma = new PrismaClient()
 
 export const GetArticles = async (req: Request, res: Response) => {
     try {
-        const articles = await prisma.article.findMany();
-        res.json(new ApiResponse(articles))
+        const articles = await prisma.article.findMany({ orderBy: { createAt: 'desc' } });
 
+        let articleMapper = await Promise.all(articles.map(async (article) => {
+
+            return {
+                ...article,
+                authorName: await getAuthor(article),
+                categoryName: await getCategory(article)
+            }
+
+        }))
+
+        res.json(new ApiResponse(articleMapper))
 
     } catch (error) {
         res.json(getErrorMessage('An error has ocurred', error))
     }
 }
 
+const getAuthor = async (article: any) => {
+    let authorName = await prisma.author.findFirst({ where: { id: article.idAuthor }, select: { name: true } })
+    console.log(`Author for article ${article.id}:`, authorName?.name);
+    return authorName?.name
+}
 
+const getCategory = async (article: any) => {
 
+    let category = await prisma.category.findFirst({ where: { id: article.idCategory }, select: { name: true } })
+    console.log(`getCategory for article ${article.id}:`, category?.name);
+    return category?.name;
+}
 
 export const GetArticleById = async (req: Request, res: Response) => {
     try {
 
-         const article = await prisma.article.findFirst({ where :{
-            id: parseInt(req.params.id)
-         }});
+        const article = await prisma.article.findFirst({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
 
-         if(!article)
+        if (!article)
             return res.status(404).json(new ApiResponse('item not found'))
 
-         return res.json(article)
+        return res.json(article)
 
     } catch (error) {
         res.json(getErrorMessage('An error has ocurred', error))
@@ -40,20 +62,24 @@ export const GetArticleById = async (req: Request, res: Response) => {
 export const DeleteAticle = async (req: Request, res: Response) => {
     try {
 
-        const article = await prisma.article.findFirst({ where :{
-            id: parseInt(req.params.id)
-         }});
+        const article = await prisma.article.findFirst({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
 
-         if(!article)
+        if (!article)
             return res.status(404).json(new ApiResponse('item not found'))
 
-         await prisma.article.delete({ where :{
-            id: parseInt(req.params.id)
-         }});
+        await prisma.article.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
 
-         res.json(new ApiResponse({message: 'item deleted'}))
+        res.json(new ApiResponse({ message: 'item deleted' }))
 
-         
+
     } catch (error) {
         res.json(getErrorMessage('An error has ocurred', error))
     }
@@ -63,7 +89,7 @@ export const CreateArticle = async (req: Request, res: Response) => {
     try {
 
         await prisma.article.create({ data: req.body })
-        res.json(new ApiResponse({message: 'item created'}))
+        res.json(new ApiResponse({ message: 'item created' }))
 
     } catch (error) {
         res.json(getErrorMessage('An error has ocurred', error))
@@ -74,16 +100,18 @@ export const CreateArticle = async (req: Request, res: Response) => {
 export const UpdateArticle = async (req: Request, res: Response) => {
     try {
 
-        const article = await prisma.article.findFirst({ where :{
-            id: parseInt(req.body.id)
-         }});
+        const article = await prisma.article.findFirst({
+            where: {
+                id: parseInt(req.body.id)
+            }
+        });
 
-        if(!article)
+        if (!article)
             return res.status(404).json(new ApiResponse('item not found'))
 
         let articleUpdate = await prisma.article.update({
             where: {
-                id : parseInt(req.body.id)
+                id: parseInt(req.body.id)
             },
             data: req.body
         })
